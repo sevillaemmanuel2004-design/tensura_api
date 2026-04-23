6a603d7d0f7bbff345f066983e4e40fcc5f07368
sevillaemmanuel2004-design <sevilla.emmanuel2004@gmail.com>
2026-04-23 13:34:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,7 +14,10 @@ def get_db():
         yield db
     finally:
         db.close()
-
+@app.get("/")
+def serve_home():
+    return FileResponse("index.html")
+    
 # 1️⃣ Get All Characters
 @app.get("/characters")
 def get_characters(db: Session = Depends(get_db)):
